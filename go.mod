03e86774a3af756bf7bb358b7f343fc858db6fee
goo987 <apaajaminum555@gmail.com>
2025-08-13 17:18:11 +0700

    add files

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module projectweb
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.2.2 // indirect
+require (
+	github.com/a-h/templ v0.3.924
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.2
+)
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"net/http"
+
+	"projectweb/views"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
-	
+	PORT := ":8080"
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		views.Index().Render(r.Context(), w)
+	})
+
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		views.Login().Render(r.Context(), w)
+	})
+
+	r.Get("/Register", func(w http.ResponseWriter, r *http.Request) {
+		views.Register().Render(r.Context(), w)
+	})
+
+	r.Get("/pengaduan", func(w http.ResponseWriter, r *http.Request) {
+		views.Pengaduan().Render(r.Context(), w)
+	})
+
+	r.Get("/listpengaduan", func(w http.ResponseWriter, r *http.Request) {
+		views.ListPengaduan().Render(r.Context(), w)
+	})
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		views.Notfound().Render(r.Context(), w)
+	})
+
+	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		views.Unautorize().Render(r.Context(), w)
+	})
 }
